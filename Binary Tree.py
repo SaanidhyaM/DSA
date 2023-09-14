b4be38422a6f4438692db30606d492828601acb9
Saanidhya Mittal <saanidhyamittal@gmail.com>
2023-09-14 16:06:39 +0530

    binary tree

---
+++ Binary Tree.py
@@ -0,0 +1,34 @@
+class User:
+    def __init__(self, username, name, email):
+        self.username = username
+        self.name = name
+        self.email = email
+        
+    def __repr__(self):
+        return "User(username='{}', name='{}', email='{}')".format(self.username, self.name, self.email)
+    
+    def __str__(self):
+        return self.__repr__()
+
+class UserDatabase:
+    def insert(self, user):
+        pass
+    
+    def find(self, username):
+        pass
+    
+    def update(self, user):
+        pass
+        
+    def list_all(self):
+        pass
+
+aakash = User('aakash', 'Aakash Rai', 'aakash@example.com')
+biraj = User('biraj', 'Biraj Das', 'biraj@example.com')
+hemanth = User('hemanth', 'Hemanth Jain', 'hemanth@example.com')
+jadhesh = User('jadhesh', 'Jadhesh Verma', 'jadhesh@example.com')
+siddhant = User('siddhant', 'Siddhant Sinha', 'siddhant@example.com')
+sonaksh = User('sonaksh', 'Sonaksh Kumar', 'sonaksh@example.com')
+vishal = User('vishal', 'Vishal Goel', 'vishal@example.com')
+
+users = [aakash, biraj, hemanth, jadhesh, siddhant, sonaksh, vishal]
